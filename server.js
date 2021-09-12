c879a98d031a55343ca44a5ba76aafe8b72feb56
Aidan Amato <aidanamato@comcast.net>
2021-09-12 15:36:03 -0400

    add CRUD routes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mysql = require('mysql2');
 
+const inputCheck = require('./utils/inputCheck');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -20,34 +22,81 @@ const db = mysql.createConnection(
 );
 
 // GET all candidates
-// db.query('SELECT * FROM candidates', (err, rows) => {
-//   console.log(rows);
-// });
+app.get('/api/candidates', (req, res) => {
+  db.query('SELECT * FROM candidates', (err, rows) => {
+    if (err) {
+      res.status(500).json({err: err.message});
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
 
 // GET a single candidate
-// db.query('SELECT * FROM candidates WHERE id = 27', (err, row) => {
-//   console.log(row);
-// });
+app.get('/api/candidates/:id', (req, res) => {
+  const sql = 'SELECT * FROM candidates WHERE id = ?';
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({err: err.message});
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
 
 // DELETE a candidate
-// db.query('DELETE FROM candidates WHERE id = ?', 1, (err, result) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result);
-// });
+app.delete('/api/candidates/:id', (req, res) => {
+  const sql = 'DELETE FROM candidates WHERE id = ?';
+  const params = [req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({err: res.message});
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
 
 // Create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//               VALUES (?, ?, ?, ?)`;
-// const params = [1, 'Ronald', 'Firbank', 1];
+app.post('/api/candidates', ({body}, res) => {
+  const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+  if (errors) {
+    res.status(400).json({error: errors});
+    return;
+  }
 
-// db.query(sql, params, (err, result) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result);
-// });
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+              VALUES (?, ?, ?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({error: err.message});
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+  });
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
